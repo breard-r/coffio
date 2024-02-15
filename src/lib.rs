1e4a875d8f8da5ebe7a97da7a690e37f8a403147
Rodolphe Bréard <v.code.ngyvz7vf@what.tf>
2024-02-15 11:09:22 +0100

    Prevent the crate to be used by accident

--- src/lib.rs
+++ src/lib.rs
@@ -8,3 +8,6 @@ pub use scheme::Scheme;
 
 const DEFAULT_IKM_DURATION: u64 = 60 * 60 * 24 * 365; // In seconds
 const DEFAULT_SCHEME: Scheme = Scheme::XChaCha20Poly1305WithBlake3;
+
+#[cfg(not(feature = "i-understand-and-accept-the-risks"))]
+compile_error!("This crate is experimental and therefore comes with absolutely no security guaranty. To use it anyway, enable the \"i-understand-and-accept-the-risks\" feature.");
